70fd32f435bc8e46d8cd9947255af2be38556546
Ebony <ebony.furr@yahoo.com>
2020-10-31 16:47:53 -0500

    commit tree 01eb3e6eb9441db0696ba7ae2764fe33e2323061

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog
+    def initialize #<add the default arguement here
+        @name = name
+        @breed = breed    #<put both things you want to initialize in this same method
+    end
+    
+    def name
+        @name
+    end
+
+    def name=(name)
+        @name = name
+    end
+    
+
+    def breed
+        @breed 
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+end
+
---
+++ lib/person.rb
@@ -0,0 +1,24 @@
+class Person
+    def initialize #<add the default arguement here
+        @name = name
+        @job = job    #<put both things you want to initialize in this same method
+    end
+    
+    def name
+        @name
+    end
+
+    def name=(name)
+        @name = name
+    end
+    
+
+    def job
+        @job 
+    end
+
+    def job=(job)
+        @job = job
+    end
+end
+
